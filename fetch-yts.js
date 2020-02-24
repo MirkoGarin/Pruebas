41dff1dd8e0f1ff18b272c913064656ab1af157f
Mirko Garin <mirkogarin@gmail.com>
2020-02-24 20:41:46 -0300

    Con Imagenes

--- fetch-yts.js
+++ fetch-yts.js
@@ -4,26 +4,33 @@
 var peli = [];
 var usuarios = [];
 var div_cine = document.querySelector("#cine");
-fetch("https://yts.lt/api/v2/list_movies.json")
+fetch("https://yts.mx/api/v2/list_movies.json")
   .then(data => data.json())
   .then(peliculas => {
 	peli = peliculas.data;
-	//console.log(JSON.stringify(peli.movies[19]['torrents'][0]['url'] ));
+	
+	console.log(peli.movies[0].medium_cover_image);
+	
 	peli.movies.map((name, i) => {
 	let nombre = document.createElement('p');
 	let url = document.createElement('a');
 	let torrent = document.createElement('a');
-      // let thumbnail = document.createElement('imag')
+	let img = document.createElement('img');
+	
 	nombre.innerHTML = i + " --  " + name.title_long;
 	url.innerHTML = name.url + "</br>" ;
-	torrent.innerHTML = name.torrents[0].url + "<hr>" ;
-
+	torrent.innerHTML = name.torrents[0].url + "<br>" ;
+	img.innerHTML = name.medium_cover_image + "</hr>" ;
+	
 	url.href = name.url  ;
+	img.href = name.medium_cover_image;
         torrent.href= name.torrents[0].url;
+	img.src =  img.href;	
 
 	div_cine.appendChild(nombre);
 	div_cine.appendChild(url);
 	div_cine.appendChild(torrent);
+	div_cine.appendChild(img);
 
     });
 
